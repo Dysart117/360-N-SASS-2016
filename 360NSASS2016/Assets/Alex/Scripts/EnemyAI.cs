ecaab8e8093ad82da1f34b1983653c07677ee177
Alex Rediker <arediker@IT400.mr.preble.com>
2016-01-04 10:29:37 -0500

    Starting AI STUFF
    What he said

---
+++ 360NSASS2016/Assets/Alex/Scripts/EnemyAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+	 
+		public Transform target;
+	public float speed = 3f;
+	
+	
+	     void Start () {
+
+		gameObject.tag = "Enemy";
+	}
+	
+	void Update(){
+		         
+			transform.LookAt(target.position);
+		transform.Rotate(new Vector3(0,-90,0),Space.Self);
+		         
+		if (Vector3.Distance(transform.position,target.position)>1f){
+			transform.Translate(new Vector3(speed* Time.deltaTime,0,0) );
+		}
+
+	}
+	
+}
