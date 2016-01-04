b27fd533d74726cef75644492e691fbd0425a295
Jacob Dawson <jadawson@utc4me.org>
2016-01-04 10:22:23 -0500

    Day #1 final commit

---
+++ 360NSASS2016/Assets/Jacob's Folder/ScoreandCombo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreandCombo : MonoBehaviour
+{
+    [SerializeField]
+    Text ScoreText;
+
+    [SerializeField]
+    float CurrentScore;
+
+    [SerializeField]
+    float Score;
+
+	void Start ()
+    {
+        ScoreText.text = "Score: " + Score;
+
+        Score = 0;
+
+        CurrentScore = Score;
+	}
+	
+	void FixedUpdate ()
+    {
+        ScoreText.text = "Score: " + CurrentScore;
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        Debug.Log("The Trigger has happened");
+
+        if (other.tag == "Player")
+        {
+            CurrentScore = CurrentScore + 10f;
+        }
+    }
+}
---
+++ 360NSASS2016/Assets/Jacob's Folder/SimpleMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour
+{
+    [SerializeField]
+    float Speed = 5f;
+
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * Speed * Time.deltaTime);
+        }
+    }
+}
